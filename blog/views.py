bee76225770ec612c404e804992bd82ee38948a5
mike840609 <mike840609@gmail.com>
2017-03-25 19:33:05 +0800

    dynamic content

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+# 有用到時間函數
+from .models import Post
+# The dot before models means current directory or current application.
+from django.utils import timezone
 
-# Create your views here.
+
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+     # In the render function we have one parameter request
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
