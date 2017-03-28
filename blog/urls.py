5d73123e931661e97966c782c913274018538aa0
mike840609 <mike840609@gmail.com>
2017-03-28 16:16:18 +0800

    post index

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 # As you can see, we're now assigning a view called post_list
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
